5d4ab03e780f270732ccaa1ce36c98d4ac2d0c12
v_weiiluo <v_weiiluo@tencent.com>
2020-11-13 10:59:01 +0800

    feature(build): 修改xhtml-ui组件引用方式 进行打包

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import hljs from 'highlight.js' //引入高亮js
 import 'highlight.js/styles/vs2015.css' //引入高亮js样式文件
-import xhtmlUi from 'xhtml-ui'
 import 'xhtml-ui/lib/xhtml-ui.css'
 // 导入默认base样式
 import './public/base.css'
@@ -17,7 +16,13 @@ export default ({
   Vue,
   option,
   router,
-  siteData
+  siteData,
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
-    Vue.use(xhtmlUi) // 设置为全局组件
+  if(!isServer){
+    import('xhtml-ui/lib/xhtml-ui.umd.min.js').then(module => {
+      Vue.use(module.default)
+    })    
+  }
+    // Vue.use(xhtmlUi) // 设置为全局组件
 }
